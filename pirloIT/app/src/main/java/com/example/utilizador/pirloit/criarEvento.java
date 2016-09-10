bc75273164ee8b1317609d8573f58cd9060216ed
JoaoMoranguinho <joao.moranguinho13@gmail.com>
2016-09-10 02:19:12 +0100

    Ativity CriaEvento

--- pirloIT/app/src/main/java/com/example/utilizador/pirloit/criarEvento.java
+++ pirloIT/app/src/main/java/com/example/utilizador/pirloit/criarEvento.java
@@ -5,8 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class criarEvento extends AppCompatActivity {
+
+    private boolean futebol = false;
+    private boolean tennis = false ;
+    private boolean volei = false ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,48 @@ public class criarEvento extends AppCompatActivity {
                 Intent backCriarIntent = new Intent(criarEvento.this,MainActivity.class);
                 startActivity(backCriarIntent);
             }
+
         });
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               futebol = true ;
+            }
+
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                volei = true ;
+            }
+
+        });
+
+
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tennis = true ;
+            }
+
+        });
+
+
+        Button confirmaCriar = (Button) findViewById(R.id.confirmaCriar);
+        confirmaCriar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent confirmaCriarIntent = new Intent(criarEvento.this,MainActivity.class);
+                startActivity(confirmaCriarIntent);
+            }
+        });
+
+
     }
 }
